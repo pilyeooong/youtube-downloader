8853a11bbbc27ecc013e25ab836ebcb970cd24b8
pilyeooong <pilyeooong@gmail.com>
2025-05-15 16:18:36 +0900

    fix. youtube downloader

---
+++ only_mp3.py
@@ -0,0 +1,16 @@
+import yt_dlp
+
+url = input("Enter YouTube URL: ")
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(title)s.%(ext)s',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',  # mp3, wav 등으로 바꿀 수 있음
+        'preferredquality': '192',
+    }],
+}
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
